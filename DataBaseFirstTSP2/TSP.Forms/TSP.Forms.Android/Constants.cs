f96ac6950bd108012ae93b883b4eb890dbb8c763
CristianCamiloAgudelo <cristian.1701322386@ucaldas.edu.co>
2020-03-09 21:06:18 -0500

    Prueba fireBAse

---
+++ DataBaseFirstTSP2/TSP.Forms/TSP.Forms.Android/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace TSP.Forms.Droid
+{
+    public static class Constants
+    {
+        public const string ListenConnectionString = "Endpoint=sb://notificationtspengennering.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=Y0vgv8F8hqkHCX3KmMzbc5nDt7I5cCLlNhTwRjWAMaw=";
+        public const string NotificationHubName = "notificationtspengennering";
+    }
+}
--- DataBaseFirstTSP2/TSP.Forms/TSP.Forms.Android/MainActivity.cs
+++ DataBaseFirstTSP2/TSP.Forms/TSP.Forms.Android/MainActivity.cs
@@ -2,20 +2,43 @@
 using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Util;
+using Android.Gms.Common;
 using Android.Runtime;
 using Xamarin.Forms;
+
 
 namespace TSP.Forms.Droid
 {
     [Activity(Label = "TSPEngennering", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+
+
+        public const string TAG = "MainActivity";
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(savedInstanceState);
+            if (Intent.Extras != null)
+            {
+                foreach (var key in Intent.Extras.KeySet())
+                {
+                    if (key != null)
+                    {
+                        var value = Intent.Extras.GetString(key);
+                        Log.Debug(TAG, "Key: {0} Value: {1}", key, value);
+                    }
+                }
+            }
+
+            IsPlayServicesAvailable();
+            CreateNotificationChannel();
+
             global::Xamarin.Forms.Forms.SetFlags("CollectionView_Experimental");
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
@@ -27,5 +50,47 @@ namespace TSP.Forms.Droid
 
         //    base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         //}
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                    Log.Debug(TAG, GoogleApiAvailability.Instance.GetErrorString(resultCode));
+                else
+                {
+                    Log.Debug(TAG, "This device is not supported");
+                    Finish();
+                }
+                return false;
+            }
+
+            Log.Debug(TAG, "Google Play Services is available.");
+            return true;
+        }
+
+        private void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channelName = CHANNEL_ID;
+            var channelDescription = string.Empty;
+            var channel = new NotificationChannel(CHANNEL_ID, channelName, NotificationImportance.Default)
+            {
+                Description = channelDescription
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
+        }
+
     }
+
 }
